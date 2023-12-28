b655ef3fb49d860cb140dec662f4a2eb8239b1f3
Binaryzero <14382+Binaryzero@users.noreply.github.com>
2023-12-28 19:14:44 +0000

    Add error handling for HTTP requests in stream_search.py

--- stream_search.py
+++ stream_search.py
@@ -30,19 +30,29 @@ if st.sidebar.button("Search"):
         # Replace commas with spaces, then split on spaces
         search_terms = [re.sub(r'\W+', '', term.strip().lower()) for term in search_terms.replace(',', ' ').split()]
 
+        # ...
         with st.spinner("Searching..."):  # Display a loading spinner
             for term in search_terms:
                 # Send GET request to the selected site
                 base_url, url_suffix = sites[site_name]
-                response = requests.get(f"{base_url}{term}{url_suffix}")
+                try:
+                    response = requests.get(f"{base_url}{term}{url_suffix}")
+                    response.raise_for_status()  # Raises stored HTTPError, if one occurred.
+                except requests.exceptions.HTTPError as http_err:
+                    st.error(f"HTTP error occurred: {http_err}")
+                except requests.exceptions.RequestException as err:
+                    st.error(f"An error occurred: {err}")
+                else:
+                    # Load response into DataFrame
+                    data = response.json()
+                    new_df = pd.DataFrame(data["entries"])
 
-
-                # Load response into DataFrame
-                data = response.json()
-                new_df = pd.DataFrame(data["entries"])
-
-                # Concatenate the new data with the existing DataFrame
-                st.session_state.df = pd.concat([st.session_state.df, new_df])
+                    # Check if the DataFrame is empty
+                    if new_df.empty:
+                        st.info(f"No results found for '{term}' on {site_name}.")
+                    else:
+                        # Concatenate the new data with the existing DataFrame
+                        st.session_state.df = pd.concat([st.session_state.df, new_df])
         st.success("Search completed.")
     else:
         st.warning("Please enter at least one search term.")
