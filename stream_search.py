81aade47147ceb50987e8991dc1608ef473b70c2
Binaryzero <14382+Binaryzero@users.noreply.github.com>
2023-12-31 00:48:20 +0000

    Refactor search functionality and add support for CVE Lookup

--- stream_search.py
+++ stream_search.py
@@ -1,20 +1,30 @@
 import re
+from pathlib import Path
 
 import pandas as pd
 import requests
 import streamlit as st
 
-HEADERS = ""
+THIS_DIR = Path(__file__).parent
+CSS_FILE = THIS_DIR / "style" / "style.css"
+
 # Set page configuration to wide layout
 st.set_page_config(
     page_title="Lookup Tool",
     initial_sidebar_state="expanded",
     layout="wide",
+    page_icon="favicon.png",
 )
 # Define a dictionary of sites to search
 sites = {
-    "Public APIs": ("https://api.publicapis.org/entries?title=", ""),
-    "Random Public APIs": ("https://api.publicapis.org/random?title=", ""),
+    "Public APIs": {
+        "url": ("https://api.publicapis.org/entries?title=", ""),
+        "headers": {"Accept": "application/json"},
+    },
+    "CVE Lookup": {
+        "url": ("https://services.nvd.nist.gov/rest/json/cves/2.0?cveId=", ""),
+        "headers": {"Accept": "application/json"},
+    },
 }
 
 # Add a drop-down for site selection in the sidebar
@@ -29,36 +39,43 @@ if st.sidebar.button("Search"):
         st.session_state.df = pd.DataFrame()
 
         # Replace commas with spaces, then split on spaces
-        search_terms = [
-            re.sub(r"\W+", "", term.strip().lower())
-            for term in search_terms.replace(",", " ").split()
-        ]
+        if site_name == "CVE Lookup":
+            # Keep hyphens for CVE Lookup
+            search_terms = [
+                re.sub(r"[^\w-]+", "", term.strip().lower())
+                for term in search_terms.replace(",", " ").split()
+            ]
+        else:
+            search_terms = [
+                re.sub(r"\W+", "", term.strip().lower())
+                for term in search_terms.replace(",", " ").split()
+            ]
 
-        # ...
-        with st.spinner("Searching..."):  # Display a loading spinner
-            for term in search_terms:
-                # Send GET request to the selected site
-                base_url, url_suffix = sites[site_name]
-                try:
-                    response = requests.get(
-                        f"{base_url}{term}{url_suffix}", headers=HEADERS, timeout=120
-                    )
-                    response.raise_for_status()  # Raises stored HTTPError, if one occurred.
-                except requests.exceptions.HTTPError as http_err:
-                    st.error(f"HTTP error occurred: {http_err}")
-                except requests.exceptions.RequestException as err:
-                    st.error(f"An error occurred: {err}")
-                else:
-                    # Load response into DataFrame
-                    data = response.json()
-                    new_df = pd.DataFrame(data["entries"])
+        # Get the base URL and URL suffix for the selected site
+        base_url, url_suffix = sites[site_name]["url"]
+        headers = sites[site_name]["headers"]
 
-                    # Check if the DataFrame is empty
-                    if new_df.empty:
-                        st.info(f"No results found for '{term}' on {site_name}.")
-                    else:
-                        # Concatenate the new data with the existing DataFrame
-                        st.session_state.df = pd.concat([st.session_state.df, new_df])
+        # Make the HTTP request
+        for term in search_terms:
+            response = requests.get(f"{base_url}{term}{url_suffix}", headers=headers)
+            # ...
+            try:
+                response.raise_for_status()  # Raises stored HTTPError, if one occurred.
+            except requests.exceptions.HTTPError as http_err:
+                st.error(f"HTTP error occurred: {http_err}")
+            except requests.exceptions.RequestException as err:
+                st.error(f"An error occurred: {err}")
+            else:
+                # Load response into DataFrame
+                data = response.json()
+            new_df = pd.DataFrame(data["entries"])
+
+            # Check if the DataFrame is empty
+            if new_df.empty:
+                st.info(f"No results found for '{term}' on {site_name}.")
+            else:
+                # Concatenate the new data with the existing DataFrame
+                st.session_state.df = pd.concat([st.session_state.df, new_df])
         # st.success("Search completed.")
     else:
         st.warning("Please enter at least one search term.")
